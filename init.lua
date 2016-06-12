faabc17d84a42ca34c30004f005c05397e31c299
tenplus1 <tenplus1@users.noreply.github.com>
2016-06-12 11:31:03 +0100

    Wont heal when poisoned
    Hearts no longer go up when poisoned.

--- init.lua
+++ init.lua
@@ -189,7 +189,10 @@ local function stamina_globaltimer(dtime)
 
 						local pos = player:getpos()
 						local node = minetest.get_node({
-							x = pos.x, y = pos.y - 1, z = pos.z})
+							x = pos.x,
+							y = pos.y - 1,
+							z = pos.z
+						})
 
 						if node.name ~= "air" then
 
@@ -251,10 +254,10 @@ local function stamina_globaltimer(dtime)
 				local air = player:get_breath() or 0
 				local hp = player:get_hp()
 
-				-- don't heal if drowning or dead
-				-- TODO: don't heal if poisoned?
+				-- don't heal if drowning, dead or poisoned
 				local h = tonumber(tab.level)
-				if h >= STAMINA_HEAL_LVL and h >= hp and hp > 0 and air > 0 then
+				if h >= STAMINA_HEAL_LVL and h >= hp and hp > 0 and air > 0
+				and tab.poison == false then
 					player:set_hp(hp + STAMINA_HEAL)
 					stamina_update(player, h - 1)
 				end
@@ -271,11 +274,13 @@ local function stamina_globaltimer(dtime)
 end
 
 local function poison_player(ticks, time, elapsed, user)
+	local name = user:get_player_name()
 	if elapsed <= ticks then
 		minetest.after(time, poison_player, ticks, time, elapsed + 1, user)
+		stamina_players[name].poison = true
 	else
-		local name = user:get_player_name()
 		user:hud_change(stamina_players[name].hud_id, "text", "stamina_hud_fg.png")
+		stamina_players[name].poison = false
 	end
 	local hp = user:get_hp() -1 or 0
 	if hp > 0 then
@@ -357,6 +362,7 @@ if minetest.setting_getbool("enable_damage") and minetest.is_yes(minetest.settin
 		stamina_players[name] = {}
 		stamina_players[name].level = stamina_read(player)
 		stamina_players[name].exhaust = 0
+		stamina_players[name].poison = false
 		local level = math.min(stamina_players[name].level, STAMINA_VISUAL_MAX)
 		local id = player:hud_add({
 			name = "stamina",
@@ -391,3 +397,4 @@ if minetest.setting_getbool("enable_damage") and minetest.is_yes(minetest.settin
 		stamina_update(player, STAMINA_VISUAL_MAX)
 	end)
 end
+
